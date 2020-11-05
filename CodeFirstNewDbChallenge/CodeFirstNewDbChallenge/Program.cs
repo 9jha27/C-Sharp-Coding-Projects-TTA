4026b31b71f9e4aec94f534550d3b7d3c0c8fa78
9jha27 <65628178+9jha27@users.noreply.github.com>
2020-11-05 13:02:45 -0700

    CodeFirst Challenge

---
+++ CodeFirstNewDbChallenge/CodeFirstNewDbChallenge/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Diagnostics.SymbolStore;
+
+namespace CodeFirstNewDbChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new SchoolContext())
+            {
+
+                DateTime dob = new DateTime(1990, 09, 27);
+                var student = new Student() { FirstName = "Laura", LastName = "Ha", DateOfBirth = dob, EmailAddress = "laura@laura.com", StudentId = 1 };
+
+                db.Students.Add(student);
+                db.SaveChanges();
+
+
+            }
+        }
+    }
+    
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string EmailAddress { get; set; }
+        
+        public Grade Grade { get; set; }
+
+    }
+
+    public class Grade
+    {
+        public int GradeId { get; set; }
+        public string GradeName { get; set; }
+        public string Section { get; set; }
+
+        public ICollection<Student> Students { get; set; }
+    }
+
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base()
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+    }
+
+
+}
---
+++ dotNetCorePublishChallenge/dotNetCorePublishChallenge/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using dotNetCorePublishChallenge.Models;
+
+namespace dotNetCorePublishChallenge.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            DateTime serverTime = new DateTime();
+            serverTime = DateTime.Now;
+            return View(serverTime);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        
+    }
+}
